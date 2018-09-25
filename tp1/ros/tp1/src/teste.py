b0ac46c4224df3d8c13b4fbded3883990baa3853
ha2398 <hsousa@gmx.com>
2018-09-25 20:30:10 -0300

    add simple differential controller

--- tp1/ros/tp1/src/teste.py
+++ tp1/ros/tp1/src/teste.py
@@ -8,10 +8,12 @@ from nav_msgs.msg import Odometry
 
 
 odomMsg = None
-target = (6, -6, pi / 2)
-kx, ky, kt = 1, 1, 1
+target = (2, 4, pi / 2)
+kx, ky, kt, kp = 1, 1, 1, 1
 tolerance = 0.001
 tolerance_a = 0.01
+
+goal = False
 
 
 def LaserCallback(msg):
@@ -33,21 +35,29 @@ def yawFromQuaternion(orientation):
     return atan2((2.0 * (w * z + x * y)), (1.0 - 2.0 * (y * y + z * z)))
 
 
-def holonomic_controller():
-    vel = Twist()
+def get_error():
     ex = target[0] - odomMsg.pose.pose.position.x
     ey = target[1] - odomMsg.pose.pose.position.y
-    print('Ex', round(ex, 4), 'Ey', round(ey, 4))
+    et = target[2] - yawFromQuaternion(odomMsg.pose.pose.orientation)
+
+    return ex, ey, et
+
+
+def holonomic_controller():
+    global goal
+
+    vel = Twist()
+    ex, ey, et = get_error()
 
     if ex < tolerance and ey < tolerance:
         vel.linear.x = 0
         vel.linear.y = 0
 
-        et = target[2] - yawFromQuaternion(odomMsg.pose.pose.orientation)
         print('Et', round(et, 4))
 
         if et < tolerance_a:
             vel.angular.z = 0
+            goal = True
         else:
             vel.angular.z = kt * et
 
@@ -58,7 +68,22 @@ def holonomic_controller():
 
 
 def differential_controller():
-    pass
+    global goal
+
+    vel = Twist()
+    ex, ey, et = get_error()
+    theta = yawFromQuaternion(odomMsg.pose.pose.orientation)
+
+    vel.linear.x = kp * ex * cos(theta)
+    vel.linear.y = kp * ey * sin(theta)
+    vel.angular.z = kt * (atan2(ey, ex) - theta)
+
+    if ex < tolerance and ey < tolerance and et < tolerance_a:
+        vel.linear.x = 0
+        vel.linear.y = 0
+        vel.angular.z = 0
+
+    return vel
 
 
 def run():
@@ -70,8 +95,9 @@ def run():
     cmd_vel = Twist()
 
     while not rospy.is_shutdown():
-        if odomMsg:
-            cmd_vel = holonomic_controller()
+        if odomMsg and not goal:
+            # cmd_vel = holonomic_controller()
+            cmd_vel = differential_controller()
 
         pub.publish(cmd_vel)
         rate.sleep()
