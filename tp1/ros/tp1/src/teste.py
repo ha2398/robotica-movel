11190bb44bc5e679e03d2031c0c836d26cc04794
ha2398 <hsousa@gmx.com>
2018-09-25 18:14:07 -0300

    add rotation for holonomic controller

--- tp1/ros/tp1/src/teste.py
+++ tp1/ros/tp1/src/teste.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import rospy
+
 from geometry_msgs.msg import Twist
+from math import sin, cos, atan2, pi
 from nav_msgs.msg import Odometry
 
 
 odomMsg = None
-target = (7, 8.123, 0)
-kx, ky, kz = 1, 1, 1
+target = (6, -6, pi / 2)
+kx, ky, kt = 1, 1, 1
+tolerance = 0.001
+tolerance_a = 0.01
 
 
 def LaserCallback(msg):
@@ -20,6 +24,43 @@ def OdomCallback(msg):
     odomMsg = msg
 
 
+def yawFromQuaternion(orientation):
+    x = orientation.x
+    y = orientation.y
+    z = orientation.z
+    w = orientation.w
+
+    return atan2((2.0 * (w * z + x * y)), (1.0 - 2.0 * (y * y + z * z)))
+
+
+def holonomic_controller():
+    vel = Twist()
+    ex = target[0] - odomMsg.pose.pose.position.x
+    ey = target[1] - odomMsg.pose.pose.position.y
+    print('Ex', round(ex, 4), 'Ey', round(ey, 4))
+
+    if ex < tolerance and ey < tolerance:
+        vel.linear.x = 0
+        vel.linear.y = 0
+
+        et = target[2] - yawFromQuaternion(odomMsg.pose.pose.orientation)
+        print('Et', round(et, 4))
+
+        if et < tolerance_a:
+            vel.angular.z = 0
+        else:
+            vel.angular.z = kt * et
+
+    vel.linear.x = kx * ex
+    vel.linear.y = ky * ey
+
+    return vel
+
+
+def differential_controller():
+    pass
+
+
 def run():
     global laserMsg
     rospy.init_node('move_example', anonymous=True)
@@ -30,19 +71,7 @@ def run():
 
     while not rospy.is_shutdown():
         if odomMsg:
-            x = odomMsg.pose.pose.position.x
-            y = odomMsg.pose.pose.position.y
-            z = odomMsg.pose.pose.position.z
-
-            ex = target[0] - x
-            ey = target[1] - y
-            ez = target[2] - z
-
-            print 'Position:\n', odomMsg.pose.pose.position, '\n'
-            print 'Error:\n', 'ex:', ex, 'ey:', ey, 'ez:', ez
-
-            cmd_vel.linear.x = kx * ex
-            cmd_vel.linear.y = ky * ey
+            cmd_vel = holonomic_controller()
 
         pub.publish(cmd_vel)
         rate.sleep()
